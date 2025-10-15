299b7bd21944cab3d7f05e4b097aea353e558e13
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-10-16 01:19:20 +0530

    path fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- Add this line
+  base: './', // Important for Vercel relative paths
 })
